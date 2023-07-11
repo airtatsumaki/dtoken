0ad141afd6954a40ddc6b59cfa75e8ca30a7d224
airtatsumaki <naz1mb20@gmail.com>
2023-07-11 16:50:02 +0100

    createRoot updated in index.jsx

--- src/dtoken_frontend/src/index.jsx
+++ src/dtoken_frontend/src/index.jsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  const root = createRoot(document.getElementById("root"));
+  root.render(<App />);
 }
 
 init();
