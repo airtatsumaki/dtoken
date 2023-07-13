2a3b2185fe20a699e8b44751ac7dda455e68a2a6
airtatsumaki <naz1mb20@gmail.com>
2023-07-13 17:22:09 +0100

    authenticate user part 1 done

--- src/dtoken_frontend/src/index.jsx
+++ src/dtoken_frontend/src/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./components/App";
+import { AuthClient } from '@dfinity/auth-client';
+
 
 const init = async () => { 
   const root = createRoot(document.getElementById("root"));
-  root.render(<App />);
+  const authClient = await AuthClient.create();
+
+  if(authClient.isAuthenticated()){
+    console.log("logged in");
+    root.render(<App />);
+    
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        root.render(<App />);
+      }
+    })
+  }
 }
 
 init();
